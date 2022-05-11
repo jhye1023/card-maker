ed2bbfca3895a15bf4858f596ff7c7b08e8e6675
jhye1023 <jihye1023@gmail.com>
2022-05-11 11:49:28 -0700

    updates

--- src/app.jsx
+++ src/app.jsx
@@ -1,23 +1,22 @@
-// import logo from './logo.svg';
-import './app.module.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import styles from "./app.module.css";
+import Login from "./components/login/login";
+import Maker from "./components/maker/maker";
 
-function App() {
+function App({ FileInput, authService, cardRepository }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <p>
-          Edit <code>src/app.jsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <BrowserRouter>
+        <Routes>
+          <Route exact path="/" element={<Login authService={authService}/>}/> 
+          <Route path="/maker" element={   <Maker
+              FileInput={FileInput}
+              authService={authService}
+              cardRepository={cardRepository}
+            />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.module.css';
-import App from './app';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import "./index.module.css";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// import reportWebVitals from "./reportWebVitals";
+
+import React, { memo } from "react";
+// import ReactDOM from 'react-dom';
+
+import App from "./app";
+import AuthService from "./service/auth_service";
+import ImageUploader from "./service/image_uploader";
+import ImageFileInput from "./components/image_file_input/image_file_input";
+import CardRepository from "./service/card_repository";
+import firebaseApp from "./service/firebase";
+
+const authService = new AuthService(firebaseApp);
+const cardRepository = new CardRepository(firebaseApp);
+const imageUploader = new ImageUploader();
+const FileInput = memo((props) => (
+  <ImageFileInput {...props} imageUploader={imageUploader} />
+));
+console.log(authService);
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      authService={authService}
+      FileInput={FileInput}
+      cardRepository={cardRepository}
+    />
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// // If you want to start measuring performance in your app, pass a function
+// // to log results (for example: reportWebVitals(console.log))
+// // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
